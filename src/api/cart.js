083681fd1087386b2209dfe320ab60b4fc779e50
hongyuChen <hongyuChen@web.cn>
2026-06-28 22:10:03 +0800

    feat:vuex中的state单独保存token

--- src/api/cart.js
+++ src/api/cart.js
@@ -2,10 +2,10 @@ import request from '@/utils/request'
 import store from '@/store/index'
 
 export const getCartList = () => {
-  if (store.state.user.userInfo.token) {
+  if (store.state.user.token) {
     return request.get('/cart/list', {
       headers: {
-        'Access-Token': store.state.user.userInfo.token,
+        'Access-Token': store.state.user.token,
         platform: 'H5'
       }
     })
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -3,12 +3,15 @@ import { getInfo, setInfo } from '@/utils/localStorge'
 export default {
   // 开启命名空间
   namespaced: true,
+  strict: true,
   state () {
     return {
-      userInfo: getInfo()
+      userInfo: getInfo(),
+      token: getInfo().token
     }
   },
   mutations: {
+    // 登录时触发
     getUserInfo (state, obj) {
       state.userInfo = obj
       setInfo(obj)
